0d760bd685da13931c68c95789394000d61d9800
runner <runner@Mac-1678178056008.local>
2023-03-07 08:35:43 +0000

    Update Package.swift to new release (v5.4.4)

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "FreshchatSDK",
-                      url: "https://github.com/kientux/freshchat-ios-spm/releases/download/v5.4.3/FreshchatSDK.xcframework.zip",
-                      checksum: "9f73b0e3b6154fe615311826474206e736099ead8ae20d820268ddbf8d39d5ff")
+                      url: "https://github.com/kientux/freshchat-ios-spm/releases/download/v5.4.4/FreshchatSDK.xcframework.zip",
+                      checksum: "75983bfb83cbbfe66e1cd39b17967d5a3227981b610acee2ba677e4796f8106e")
     ]
 )
